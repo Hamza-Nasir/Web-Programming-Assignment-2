6958ba95157e99daf26bf2c6dc3a4df5e13b5238
Hamza-Nasir <hamzanasir123123@gmail.com>
2023-01-29 13:35:53 +0500

    Added: Image upload, stuff hidden until image uploaded

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Hello!")
+function chooseImage() {
+    const input = document.getElementById("file-input");
+    input.addEventListener("change", function() {
+        const file = input.files[0];
+        const reader = new FileReader();
+        reader.onload = function() {
+            const img = document.getElementById("selected-image");
+            img.src = reader.result;
+        }
+        reader.readAsDataURL(file);
+
+        const filterBox = document.getElementsByClassName("filter-container")[0]
+        const imageBox = document.getElementsByClassName("image-container")[0]
+
+        filterBox.style.display = "block";
+        imageBox.style.display = "block";
+    });
+    input.click();
+}
+
