8c2c9671cdccd987b38b5083b965cfdb7b637142
Hamza-Nasir <hamzanasir123123@gmail.com>
2023-01-30 10:41:57 +0500

    Filters working

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 var filters = ["brightness-button", "saturation-button", "inversion-button", "grayscale-button"]
 var filterHeadings = ["Brightness", "Saturation", "Inversion", "Grayscale"]
+var filterValues = [100, 100, 0, 0]
 var selectedElement = 0;
+
+function setFilterValue() {
+    var slider = document.getElementById('rangeSlider');
+    slider.addEventListener('input', sliderChange);
+
+    function sliderChange() {
+        document.getElementById("slider-value").innerHTML = this.value;
+        filterValues[selectedElement] = this.value;
+
+        if (selectedElement == 0) {
+            document.getElementById("selected-image").style.filter = `brightness(${this.value}%) saturate(${filterValues[1]}%) invert(${filterValues[2]}%) grayscale(${filterValues[3]}%)`;
+        } else if (selectedElement == 1) {
+            document.getElementById("selected-image").style.filter = `brightness(${filterValues[0]}%) saturate(${this.value}%) invert(${filterValues[2]}%) grayscale(${filterValues[3]}%)`;
+        } else if (selectedElement == 2) {
+            document.getElementById("selected-image").style.filter = `brightness(${filterValues[0]}%) saturate(${filterValues[1]}%) invert(${this.value}%) grayscale(${filterValues[3]}%)`;
+        } else if (selectedElement == 3) {
+            document.getElementById("selected-image").style.filter = `brightness(${filterValues[0]}%) saturate(${filterValues[1]}%) invert(${filterValues[2]}%) grayscale(${this.value}%)`;
+        }
+    }
+}
 
 function chooseImage() {
     const input = document.getElementById("file-input");
@@ -27,6 +48,12 @@ function chooseFilter(itemNo) {
         return
     }
 
+    if (itemNo != 0) {
+        document.getElementById("rangeSlider").max = 100
+    } else {
+        document.getElementById("rangeSlider").max = 200
+    }
+
     newItem = document.getElementById(filters[itemNo])
     oldItem = document.getElementById("selected-filter-button")
 
@@ -38,4 +65,7 @@ function chooseFilter(itemNo) {
     // Setting the name for the slider to also display
     sliderHeading = document.getElementById("filter-name")
     sliderHeading.innerHTML = filterHeadings[selectedElement]
+
+    document.getElementById("slider-value").innerHTML = filterValues[selectedElement];
+    document.getElementById("rangeSlider").value = filterValues[selectedElement];
 }
